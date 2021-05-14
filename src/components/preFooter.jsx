7c8cbd38587a932e7eb50d7ced6138f595472a80
Cankarnama <69017737+Cankarnama@users.noreply.github.com>
2021-05-14 19:15:57 +0000

    added pre footer section

---
+++ src/components/preFooter.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const PreFooter = () => {
+    return (
+        <section className="prefooter">
+            <div className="info">
+                <h2>Earn up to $25 worth <br /> of crypto</h2>
+                <p>Discover how specific cryptocurrencies work — and <br /> get a bit of each crypto to try out for yourself.</p>
+                <a href="#" className="btn">Start earning</a>
+            </div>
+            <div className="image">
+                <img src="/img/space-crypto.png" alt="" />
+            </div>
+        </section>
+    );
+}
+
+export default PreFooter;
