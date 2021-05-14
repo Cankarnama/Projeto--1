b52a56934c181f54510aad477a1c9dbbd00d8a7f
Cankarnama <69017737+Cankarnama@users.noreply.github.com>
2021-05-14 19:18:53 +0000

    imported section components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import './App.css';
 import React, { Fragment } from 'react';
+import AppHeader from './components/appHeader';
+import HeroSection from './components/heroSection';
+import CryptotableSection from './components/cryptotableSection';
+import EarnSection from './components/earnSection';
+import CreateCryptoSection from './components/createCryptoSection';
+import Trust from './components/trust';
+import Stats from './components/stats';
+import GetStarted from './components/getStarted';
+import PreFooter from './components/preFooter';
+import Footer from './components/footer';
 
 function App() {
     return (<div>Hello world</div>)
