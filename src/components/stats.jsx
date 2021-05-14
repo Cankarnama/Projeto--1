52adcf4eeeb503713a1c02632659e65acc16baf0
Safo-Prince <55955810+Safo-Prince@users.noreply.github.com>
2021-05-14 19:12:51 +0000

    created crypto statistics section

---
+++ src/components/stats.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Stats = () => {
+    return (
+        <section className="stats">
+            <div className="stat">
+                <h1>$335B</h1>
+                <p>Quarterly volume traded</p>
+            </div>
+            <div className="stat">
+                <h1>100+</h1>
+                <p>Countries supported</p>
+            </div>
+            <div className="stat">
+                <h1>56+M</h1>
+                <p>Verified users</p>
+            </div>
+        </section>
+    );
+}
+
+export default Stats;
